b4f34cf5733f887b18097afc4ce920fd9d9f63ee
Kuznetsov Mikhail <mi.kuznetsov@tinkoff.ru>
2024-11-07 21:07:58 +0300

    Добавлен комментарий остановки

--- catprinter.py
+++ catprinter.py
@@ -1,10 +1,14 @@
 from printer import pretty_print
 
 def draw_cat():
+<<<<<<< HEAD
     cat = r"""
  /\_/\      /\–/\      /\_/\
 ( o.o )    |.>ω<.|    ฅ •ﻌ• ฅ
  > ^ <      \___/      \___/
+
+(press ctr+C to stop)
+>>>>>>> 7e9cee3 (Добавлен комментарий остановки)
 """
     return cat
 
