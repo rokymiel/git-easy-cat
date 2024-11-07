d455e02b3bdaa27a0eb07999e3a8dc345b5e4d91
Kuznetsov Mikhail <mi.kuznetsov@tinkoff.ru>
2024-11-07 20:39:02 +0300

    Добавили больше котов

--- catprinter.py
+++ catprinter.py
@@ -1,11 +1,11 @@
 from printer import pretty_print
 
 def draw_cat():
-    cat = r"""
- /\_/\
-( o.o )
- > ^ <
-"""
+        cat = r"""
+     /\_/\      /\–/\      /\_/\
+    ( o.o )    |.>ω<.|    ฅ •ﻌ• ฅ
+     > ^ <      \___/      \___/
+    """
     return cat
 
 def draw_cats(num_cats):
