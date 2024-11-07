0ae0849acfe29b025aa968598455aae407f18b82
Kuznetsov Mikhail <mi.kuznetsov@tinkoff.ru>
2024-11-07 20:26:54 +0300

    Исправлены константы цветов

--- printer.py
+++ printer.py
@@ -4,10 +4,10 @@ colors = [
     "\033[91m",  # Red
     "\033[92m",  # Green
     "\033[93m",  # Yellow
-    "-\033[94m",  # Blue
+    "\033[94m",  # Blue
     "\033[95m",  # Magenta
     "\033[96m",  # Cyan
-    "-\033[97m",  # White
+    "\033[97m",  # White
 ]
 reset_color = "\033[0m"
 
