0122f1b7f44c9e9146e318587924edc572282396
Kuznetsov Mikhail <mi.kuznetsov@tinkoff.ru>
2024-11-07 20:24:37 +0300

    Add cute cat ASCII art
    # Conflicts:
    #	printer.py

--- printer.py
+++ printer.py
@@ -1,6 +1,17 @@
 import random
 
 colors = [
+    "\033[91m",  # Red
+    "\033[92m",  # Green
+    "\033[93m",  # Yellow
+    "-\033[94m",  # Blue
+    "\033[95m",  # Magenta
+    "\033[96m",  # Cyan
+    "-\033[97m",  # White
+]
+reset_color = "\033[0m"
+
+colors = [
     "\033[91m",     # Red
     "\033[92m",     # Green
     "\033[93m",     # Yellow
