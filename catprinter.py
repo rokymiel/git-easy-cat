8986d3628c31c7ecca349fe66d133ff3713dad78
kAmAZ07 <killerartem30@gmail.com>
2025-10-07 12:17:39 +0300

    Добавили больше котов

--- catprinter.py
+++ catprinter.py
@@ -2,13 +2,15 @@ from printer import pretty_print
 
 def draw_cat():
     cat = r"""
- /\_/\
-( o.o )
- > ^ <
+ /\_/\    /\_/\    /\_/\
+( o.o )  ( o.o )  ( o.o )
+ > ^ <    > ^ <    > ^ <
 """
     return cat
 
 def draw_cats(num_cats):
+    if num_cats <= 0:
+        return
     for i in range(num_cats):
         pretty_print(draw_cat())
 
