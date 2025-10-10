3cda7cd9e8c9c870a30de9031b3ad30264862037
vlad250906 <vlad250906@gmail.com>
2025-10-10 19:47:36 +0500

    Исправлены константы цветов

--- printer.py
+++ printer.py
@@ -1,13 +1,13 @@
 import random
 
 colors = [
-    "\033[91m",     # Red
-    "\033[92m",     # Green
-    "\033[93m",     # Yellow
-    "-\033[94m",    # Blue
-    "\033[95m",     # Magenta
-    "\033[96m",     # Cyan
-    "-\033[97m",    # White
+    "\033[91m",  # Red
+    "\033[92m",  # Green
+    "\033[93m",  # Yellow
+    "\033[94m",  # Blue
+    "\033[95m",  # Magenta
+    "\033[96m",  # Cyan
+    "\033[97m",  # White
 ]
 reset_color = "\033[0m"
 
