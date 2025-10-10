1f848cd6facb5866469ac7295f0f5bdfd804a585
Yakomissarova <yakomissarova25@gmail.com>
2025-10-10 16:12:35 +0300

    Resolve merge conflict in colors

--- catprinter.py
+++ catprinter.py
@@ -2,17 +2,14 @@ from printer import pretty_print
 
 def draw_cat():
     cat = r"""
-<<<<<<< HEAD
- /\_/\    /\_/\    /\_/\
-( o.o )  ( o.o )  ( o.o )
- > ^ <    > ^ <    > ^ <
-=======
+
+
+
  /\_/\
 ( o.o )
  > ^ <
 
-(press ctr+C to stop)
->>>>>>> feature-loop
+
 """
     return cat
 
